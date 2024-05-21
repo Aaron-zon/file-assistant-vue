976e59b37f1f0bab0bfdc2e6a57f86ba164098bc
dl_zhoujunyang <1640485761@qq.com>
2024-05-21 17:52:33 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,88 +3,74 @@ import { ref, onMounted } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import axios from 'axios'
 
-const imgSrc = ref(null)
 onMounted(() => {
-  axios.get('http://localhost:8080/getEFile')
+  axios.get('http://localhost:8080/getFolderStructure')
     .then((resp) => {
       console.log(resp.data.data)
-      // imgSrc.value = resp.data.data[0].previewContent
+      data.value = resp.data.data
     })
 })
+const columns = [
+  {
+    key: 'name',
+    dataKey: 'fileName',
+    title: 'Name',
+    width: 150,
+  },
+  {
+    key: 'size',
+    dataKey: 'fileSize',
+    title: 'Size',
+    width: 150,
+  },
+  {
+    key: 'date',
+    dataKey: 'updateDate',
+    title: 'Last update date',
+    width: 150,
+  }
+]
+const data = ref([
+  {
+    fileName: '123',
+    fileSize: '1554k',
+    date: '2023-01-01'
+  }
+])
 </script>
 
 <template>
-  <div class="aa">
-    {{ 'asd' }}
-    <!-- <img :src="`data:image/jpeg;base64,${imgSrc}`" alt=""> -->
+  <div class="wrapper">
+    <div class="box-left"></div>
+    <div class="box-right">
+      <div class="breadcrumb">
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+          <el-breadcrumb-item>
+            <a href="/">promotion management</a>
+          </el-breadcrumb-item>
+          <el-breadcrumb-item>promotion list</el-breadcrumb-item>
+          <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+        </el-breadcrumb>
+
+      </div>
+
+      <div class="table-cantainer">
+        <el-table-v2
+          :columns="columns"
+          :data="data"
+          :width="700"
+          :height="400"
+          fixed
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.aa {
-  width: 100px;
-  height: 100px;
-  background: red;
-}
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+.wrapper {
+  border: 1px solid black;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
